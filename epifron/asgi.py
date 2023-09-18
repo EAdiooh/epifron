02a27b39b7ffed505b493e12e007e8a7227314e9
EADIooh <benji.esco@hotmail.fr>
2023-09-18 15:55:31 +0200

    first commit

---
+++ epifron/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for epifron project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epifron.settings')
+
+application = get_asgi_application()
---
+++ tickets/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class TicketStatus(models.TextChoices):
+    TO_DO = 'To Do'
+    In_PROGRESS = 'In Progress'
+    BLOCKED = 'Blocked'
+    IN_REVIEW = 'In Review'
+    DONE = 'Done'
+
+class Ticket(models.Model):
+    subject = models.CharField(max_length=100)
+    requestor = models.ForeignKey(User, related_name="requestor", null=True, blank = True, on_delete=models.CASCADE)
+    description = models.TextField()
+    responsible = models.ForeignKey(User, related_name="responsible", null=True, blank = True, on_delete=models.CASCADE)
+    assignee = models.ForeignKey(User, related_name="assignee", null=True, blank = True, on_delete=models.CASCADE)
+    status = models.CharField(max_length=25, choices=TicketStatus.choices, default=TicketStatus.TO_DO)
+    created_at = models.DateTimeField("created at")
+    last_update = models.DateTimeField("last update")
+
